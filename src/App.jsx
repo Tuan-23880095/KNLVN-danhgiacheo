b0a692b86d8e0d941071a382d5b8b18bef5574b3
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2026-03-12 15:01:46 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PeerReviewApp from './components/PeerReviewApp';
+
+function App() {
+  return (
+    <div className="App">
+      <PeerReviewApp />
+    </div>
+  );
+}
+
+export default App;
