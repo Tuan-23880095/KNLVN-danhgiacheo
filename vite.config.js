94086a49329abb6a88d1209f76e65a65924759c3
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2026-03-12 19:42:31 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Thêm dòng base này vào, tên phải khớp chính xác với tên repository
-  base: '/KNLVN-danhgiacheo/',
+  base: './', 
 })
